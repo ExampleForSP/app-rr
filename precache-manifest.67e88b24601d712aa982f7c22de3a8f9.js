720c88f9589a43a732dfd1c47d2b9e0493669242
Bartek Borowczyk <bartek@websamuraj.pl>
2019-03-25 10:52:05 +0100

    Updates

---
+++ precache-manifest.67e88b24601d712aa982f7c22de3a8f9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3037d992fb489adb11e5",
+    "url": "/app-rr/static/css/main.b018c3dc.chunk.css"
+  },
+  {
+    "revision": "3037d992fb489adb11e5",
+    "url": "/app-rr/static/js/main.3037d992.chunk.js"
+  },
+  {
+    "revision": "9873500cbc742282ea50",
+    "url": "/app-rr/static/js/runtime~main.9873500c.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/app-rr/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "34505eb256da6fbc115a5d730aad4996",
+    "url": "/app-rr/index.html"
+  }
+];
